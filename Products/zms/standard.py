a23d182a4ba74db30923f5301889abba72b04dcf
Artification AG <dev@artification.com>
2022-07-07 13:47:48 +0200

    url params with & instead of &amp;

--- Products/zms/standard.py
+++ Products/zms/standard.py
@@ -279,7 +279,7 @@ def umlaut_quote(s, mapping={}):
 
 
 security.declarePublic('url_append_params')
-def url_append_params(url, dict, sep='&amp;'):
+def url_append_params(url, dict, sep='&'):
   """
   Append params from dict to given url.
   @param url: Url
@@ -323,7 +323,7 @@ def url_append_params(url, dict, sep='&amp;'):
 
 
 security.declarePublic('url_inherit_params')
-def url_inherit_params(url, REQUEST, exclude=[], sep='&amp;'):
+def url_inherit_params(url, REQUEST, exclude=[], sep='&'):
   """
   Inerits params from request to given url.
   @param url: Url
